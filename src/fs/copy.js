f6807f1dffe49eab2048dff6145ab1f2bd8ee671
AlexK <kivlaros@gmail.com>
2025-04-24 18:15:22 +0300

    finish fs copy task

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,18 @@
+import { promises as fs } from 'fs';
+
 const copy = async () => {
-    // Write your code here 
+    try{
+        const filesFolderStats = await fs.stat('files')
+        const filesCopyFolderStats = await fs.stat('files_copy')
+        if(!filesFolderStats.isDirectory()||filesCopyFolderStats.isDirectory()){
+            console.log(filesFolderStats.isDirectory(),filesCopyFolderStats.isDirectory())
+            throw new Error('FS operation failed')
+        }
+        await fs.mkdir('files_copy');
+        await fs.cp('files', 'files_copy',{ recursive: true });
+    }catch(err){
+     console.log(err)
+    }
 };
 
 await copy();
