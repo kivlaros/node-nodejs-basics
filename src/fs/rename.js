93629639ced0558a0318fe878eb1f8850537c4df
AlexK <kivlaros@gmail.com>
2025-04-26 15:47:19 +0300

    finish rename tesk

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,25 @@
+import { promises as fs } from 'fs'
+import { access, constants } from 'node:fs/promises';
+
+const filePath = './files/wrongFilename.txt'
+const newFilePath = './files/properFilename.md'
+const errorMessage = 'FS operation failed'
+
 const rename = async () => {
-    // Write your code here 
+    try{
+        await errorHandler(filePath, newFilePath)
+        await fs.rename(filePath,newFilePath)
+    }catch(err){
+        console.log(err)
+    }
 };
 
 await rename();
+
+async function errorHandler(filePath,newFilePath){
+    const isWrongExist = await access(filePath, constants.F_OK).then(() => true).catch(() => false);
+    const isMDExist = await access(newFilePath, constants.F_OK).then(() => true).catch(() => false);
+    if(!isWrongExist||isMDExist){
+        throw new Error(errorMessage)
+    }
+}
