97d251ffff56a3a5ed81ae32ad763b61329181ff
AlexK <kivlaros@gmail.com>
2025-04-26 13:54:23 +0300

    finish list task

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,24 @@
+import { promises as fs } from 'fs'
+import { access, constants } from 'node:fs/promises';
+
+const folderPath = './files'
+const errorMessage = 'FS operation failed'
+
 const list = async () => {
-    // Write your code here 
+    try{
+        await errorHandler(folderPath)
+        const filesList =  await fs.readdir(folderPath)
+        console.log(filesList)
+    }catch(err){
+        console.log(err)
+    }
 };
 
 await list();
+
+async function errorHandler(folderPath){
+    const isExist = await access(folderPath, constants.F_OK).then(() => true).catch(() => false);
+    if(!isExist){
+        throw new Error(errorMessage)
+    }
+}
