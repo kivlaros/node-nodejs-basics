fd4b20190f0811f922ea806dfac36a33eaa13965
AlexK <kivlaros@gmail.com>
2025-04-26 12:13:40 +0300

    finsh create task

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,23 @@
+import { promises as fs } from 'fs'
+import { access, constants } from 'node:fs/promises';
+
+const filePath = './files/fresh.txt'
+const errorMessage = 'FS operation failed'
+
 const create = async () => {
-    // Write your code here 
+    try{
+        await errorHandler(filePath)
+        await fs.writeFile(filePath,'I am fresh and young','utf8')
+    }catch(err){
+        console.log(err)
+    }
 };
 
 await create();
+
+async function errorHandler(filePath){
+    const isExist = await access(filePath, constants.F_OK).then(() => true).catch(() => false);
+    if(isExist){
+        throw new Error(errorMessage)
+    }
+}
