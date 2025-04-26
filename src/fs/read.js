b6de59e3337a415644f5302cb62a4137ae6a7b05
AlexK <kivlaros@gmail.com>
2025-04-26 14:34:50 +0300

    finsh read task

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,24 @@
+import { promises as fs } from 'fs'
+import { access, constants } from 'node:fs/promises';
+
+const filePath = './files/fileToRead.txt'
+const errorMessage = 'FS operation failed'
+
 const read = async () => {
-    // Write your code here 
+    try{
+        await errorHandler(filePath)
+        const content = await fs.readFile(filePath, 'utf8');
+        console.log(content)
+    }catch(err){
+        console.log(err)
+    }
 };
 
 await read();
+
+async function errorHandler(filePath){
+    const isExist = await access(filePath, constants.F_OK).then(() => true).catch(() => false);
+    if(!isExist){
+        throw new Error(errorMessage)
+    }
+}
