73a2de75b14aeadaa1f0e5494eb41c90c453ee0c
Alex <kivlaros@gmail.com>
2025-04-27 21:31:15 +0300

    finish read stream task

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,12 @@
+import { createReadStream } from 'fs';
+
 const read = async () => {
-    // Write your code here 
+    const readStream = createReadStream('./files/fileToRead.txt', 'utf8');
+    readStream.pipe(process.stdout);
+    readStream.on('error', (error) => {
+        console.error(`Read error: ${error.message}`);
+        process.exit(1);
+      });
 };
 
 await read();
