04dc142ab7a0a59ba1614fd8c23366247742d4bf
Alex <kivlaros@gmail.com>
2025-04-27 21:13:00 +0300

    finish hash task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,19 @@
-const calculateHash = async () => {
-    // Write your code here 
-};
+import { createReadStream } from 'fs';
+import { createHash } from 'crypto';
 
+const calculateHash = async () => {
+    try {
+        const readStream = createReadStream('./files/fileToCalculateHashFor.txt');
+        const hash = createHash('sha256');
+    
+        readStream
+          .on('data', (chunk) => hash.update(chunk))
+          .on('end', () => {
+            const hexHash = hash.digest('hex');
+            console.log(hexHash);
+          })
+        } catch (err) {
+            console.log(err)
+        }
+    }
 await calculateHash();
