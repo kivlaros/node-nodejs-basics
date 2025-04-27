86fc51c07dcb2b4e401e6e7c22bbe8b6b9e359c7
Alex <kivlaros@gmail.com>
2025-04-27 18:57:27 +0300

    finish cli task

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,14 @@
 const parseArgs = () => {
-    // Write your code here 
+    const args = process.argv
+    console.log(args)
+    const argsPropArray = args.reduce((acc,elem,i,arr)=>{
+        if(elem.startsWith('--')){
+            const propElem = [elem.slice(2),arr[i+1]].join(' is ')
+            acc.push(propElem)
+        }
+        return acc
+    },[]).join(', ')
+    console.log(argsPropArray)
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,16 @@
 const parseEnv = () => {
-    // Write your code here 
-};
-
+    //process.env['RSS_VARIABLE'] = 'value';
+    //process.env['RSS_TEST'] = '128'
+        
+    const envVars = process.env;
+        
+    const rssArray = Object.entries(envVars)
+            .filter(([key]) => key.startsWith('RSS_'));
+        
+    const result = rssArray
+        .map(([key, value]) => `${key}=${value}`)
+        .join('; ');
+        
+    console.log(result);
+}
 parseEnv();
