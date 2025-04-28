fa55330f03ca787bc1f1ba4688bb938a95d7e5de
AlexK <kivlaros@gmail.com>
2025-04-28 15:41:35 +0300

    finsh decompress task

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,20 @@
+import { createReadStream, createWriteStream } from 'fs';
+import { createGunzip } from 'zlib';
+
 const decompress = async () => {
-    // Write your code here 
+    const readStream = createReadStream('./files/archive.gz');
+    const writeStream = createWriteStream('./files/fileToCompress.txt');
+    const gunzip = createGunzip();
+
+    readStream
+    .pipe(gunzip)
+    .pipe(writeStream)
+    .on('finish', () => console.log('Decompression complete!'))
+    .on('error', (error) => {
+      console.error('Decompression error:', error);
+      process.exit(1);
+    })
+
 };
 
 await decompress();
