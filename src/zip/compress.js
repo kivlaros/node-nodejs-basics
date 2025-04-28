59f2264261d61ee45e7c9401bfab9b243e2bccd0
AlexK <kivlaros@gmail.com>
2025-04-28 15:31:25 +0300

    finish compress task

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,21 @@
+import { createReadStream, createWriteStream } from 'fs';
+import { createGzip } from 'zlib';
+
 const compress = async () => {
-    // Write your code here 
+    const readStream = createReadStream('./files/fileToCompress.txt');
+    const writeStream = createWriteStream('./files/archive.gz');
+    const gzip = createGzip();
+
+    readStream
+    .pipe(gzip)
+    .pipe(writeStream)
+    .on('finish', () => console.log('Compression complete!'))
+    .on('error', (error) => {
+      console.error('Compression error:', error);
+      process.exit(1);
+    });
+
+
 };
 
 await compress();
