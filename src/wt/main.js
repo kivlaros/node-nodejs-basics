08775144a44a10ce27e5be7c7c60baf415c43822
Alex <kivlaros@gmail.com>
2025-04-28 22:47:56 +0300

    finish worker task

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,32 @@
+import { Worker } from 'worker_threads';
+import os from 'os';
+
 const performCalculations = async () => {
-    // Write your code here
+    const coresCount = os.cpus().length
+    const workerPromiseArr = []
+    for(let i=0;i<coresCount;i++){
+        workerPromiseArr.push(runWorker(i,10))
+    }
+    const workerPromiseArrData = await Promise.all(workerPromiseArr)
+    console.log(workerPromiseArrData)
 };
 
 await performCalculations();
+
+function runWorker(workerIndex, initialValue){
+    return new Promise((res)=>{
+        const worker = new Worker('./worker.js')
+        worker.postMessage(initialValue + workerIndex)
+        worker.on('message', (result) => {
+            res({ ...result});
+          })
+        worker.on('error', () => {
+            res({ status: 'error', data: null});
+        })
+        worker.on('exit', (code) => {
+            if (code !== 0) {
+                res({ status: 'error', data: null});
+            }
+        })
+    })
+}
